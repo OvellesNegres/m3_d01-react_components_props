460e340bab8f5bfcbb39d85b6a04528608cfca2a
marco-ih-bcn <marco.santonastasi@ironhack.com>
2021-11-25 06:53:25 +0100

    ckeaned App.js imports

--- src/App.js
+++ src/App.js
@@ -1,11 +1,4 @@
 import "./App.css";
-import Navbar from './components/Navbar';
-import Greeting from './components/Greeting';
-import StudentCard from './components/StudentCard';
-import Badge from './components/Badge';
-import StudentList from './components/StudentList';
-
-import ReactPlayer from 'react-player';
 
 function App() {
 
