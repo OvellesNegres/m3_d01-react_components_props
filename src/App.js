b5cc8aae92169d9b9c62f5b1e225f34b0fafe56c
marco-ih-bcn <marco.santonastasi@ironhack.com>
2021-11-25 06:52:05 +0100

    delete components

--- src/App.js
+++ src/App.js
@@ -10,21 +10,7 @@ import ReactPlayer from 'react-player';
 function App() {
 
   return (
-    <div className="App">
-      <Navbar />
-      <Greeting firstName="Harper" />
-      <Greeting firstName="Michelle" />
-      <Greeting firstName="Andrea" />
-      
-      <Badge badgeText="important" />
-      
-      <StudentList>
-        <StudentCard name="Eva" week={7} info={{ city: 'BCN', course: "WEB" }} />
-        <StudentCard name="Mat" week={8} info={{ city: 'MIA', course: "DATA" }} />
-      </StudentList>
-
-            <ReactPlayer url="https://vimeo.com/channels/top/22439234" playing />
-    </div>
+    <div className="App">Hello Ironhackers!</div>
   );
 }
 
